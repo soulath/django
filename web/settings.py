d99838ba64fa710180bd5914abdf26834d48008c
soulath <96641108+soulath@users.noreply.github.com>
2023-07-19 23:32:18 +0700

    up

--- web/settings.py
+++ web/settings.py
@@ -142,6 +142,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- web/urls.py
+++ web/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     path('', include('apps.urls'))
 
 ]
-if settings.DEBUG:  # new
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
