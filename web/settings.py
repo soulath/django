2e98da383e90442d5e90194dcd14233150ff059b
soulath <96641108+soulath@users.noreply.github.com>
2023-07-25 22:10:36 +0700

    new3

--- web/settings.py
+++ web/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'web/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
